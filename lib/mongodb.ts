0072b6225b0b48fde7d77fb2e87592052fbe31fd
Giray <girayvalery@gmail.com>
2026-05-16 19:37:05 +0300

    fix mongodb SSL with default options

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,36 +1,26 @@
-import { MongoClient, MongoClientOptions } from "mongodb";
+import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI;
-const options: MongoClientOptions = {
-  maxPoolSize: 10,
-  minPoolSize: 1,
-  maxIdleTimeMS: 60000,
-  serverSelectionTimeoutMS: 10000,
-  socketTimeoutMS: 45000,
-};
+const uri = process.env.MONGODB_URI
 
 if (!uri) {
-  throw new Error(
-    "MONGODB_URI tanımlı değil. .env.local veya Vercel env variables kontrol et.",
-  );
+  throw new Error('MONGODB_URI tanımlı değil.')
 }
 
-// Global cache (hem dev hem production için)
-const globalAny = globalThis as any;
+const globalAny = globalThis as any
 
-let clientPromise: Promise<MongoClient>;
+let clientPromise: Promise<MongoClient>
 
 if (!globalAny._mongoClientPromise) {
-  const client = new MongoClient(uri, options);
-  globalAny._mongoClientPromise = client.connect();
+  const client = new MongoClient(uri)
+  globalAny._mongoClientPromise = client.connect()
 }
-clientPromise = globalAny._mongoClientPromise;
+clientPromise = globalAny._mongoClientPromise
 
 export default function getClientPromise(): Promise<MongoClient> {
-  return clientPromise;
+  return clientPromise
 }
 
 export async function getDb() {
-  const client = await clientPromise;
-  return client.db("girayoloji");
+  const client = await clientPromise
+  return client.db('girayoloji')
 }
